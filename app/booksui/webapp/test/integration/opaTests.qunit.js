d42b74bdd377bedb8af7a309b85c4a3d60ddd6a8
Filipi Fortunato <flf.2008brasil@hotmail.com>
2026-01-26 00:55:04 -0300

    first commit

---
+++ app/booksui/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'booksui/test/integration/FirstJourney',
+		'booksui/test/integration/pages/BooksList',
+		'booksui/test/integration/pages/BooksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, BooksList, BooksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('booksui') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheBooksList: BooksList,
+					onTheBooksObjectPage: BooksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
